6e9c9691e1ed5ead1b31445dc16258960d6ff607
sanket <sanket.o@media.net>
2023-05-23 14:27:02 +0530

    updates

--- db-queries/Compliance - live.sql
+++ db-queries/Compliance - live.sql
@@ -1,10 +1,11 @@
-set @uid = 80056441 ;
-select * from max_leadgen_sponsor_ad where user_id = @uid ;
+set @uid = 86893186 ;
+select * from max_leadgen_sponsor_ad where user_id = @uid order by lead_id, offer_sold_at ;
 select * from tcpa_survey_optin where user_id = @uid ;
 
--- select * from user_master where user_id = @uid ;
+select * from user_master where user_id = @uid ;
+select * from user_master_pii where user_id = @uid ;
 
--- select * from max_leadgen_sponsor_ad where stats_date > '2023-03-17' 
--- and (url_lead_status like '%Auto%' or url_lead_status like '%auto%') order by created_on desc;
+/* select * from max_leadgen_sponsor_ad where stats_date > '2023-03-17' 
+and (url_lead_status like '%Auto%' or url_lead_status like '%auto%') order by created_on desc; */
+
 -- select user_id from user_master where user_cookie_id = 'f78c1bb9-20d6-4fe4-9376-1a45885564c3'
--- 75573441 3 lead ids
