d4cd0b3334d398e7b3d5a36f029f3967faefce0b
sanket-media <sanket.o@media.net>
2023-05-24 14:22:45 +0530

    u

--- db-queries/CCPA-live.sql
+++ db-queries/CCPA-live.sql
@@ -1,8 +1,9 @@
--- Set @mail = 'vidipi4260@gam1fy.com' ;
-Set @uid = 81664719 ;
+-- Set @mail = 'jateke6133@meidecn.com' ;
+Set @uid = 84791146 ;
 select * from user_master where user_id = @uid ;
 select * from user_master_pii where user_id = @uid ;
-select * from max_leadgen_sponsor_ad where user_id = @uid ;
-select * from tcpa_survey_optin where user_id = @uid ;
+select user_id, lead_id, request_url, request_data, responce_url, ping_url_status, url_lead_status, offer_sold_at from max_leadgen_sponsor_ad where user_id = @uid ;
+-- select * from tcpa_survey_optin where user_id = @uid ;
 select * from onetrust_requests where user_id = @uid ;
 -- select * from leadgen_unsubscribed_list where email_hash = md5(@mail) ;
+
