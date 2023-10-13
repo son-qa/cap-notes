601e25e864b6e8314f88051eb4d5a52e424a83a1
sanket <sanket.o@media.net>
2023-10-13 21:12:41 +0530

    updates

--- db-queries/disclaimer_experiments.sql
+++ db-queries/disclaimer_experiments.sql
@@ -1,13 +1,17 @@
 select experiment_slug, experiment_name, experiment_type, experiment_schema, config_data from ab_experiment_v2 where site_id = 1
 and experiment_slug not in 
-('FSPU-AI-SS-Branded-Dynamic-Flow', 'FSPU-Branded-Personalized-Appwall') ;
+('FSPU-AI-SS-Branded-Dynamic-Flow', 'FSPU-Branded-Personalized-Appwall', 'FSPU-Branded-Personalized-SS-V3-LP', 'FSPU-Facebook-Dynamic-Prelander', 'FSPU-Generic-Pre-filled-Reg',
+'')
+order by experiment_type desc ;
 
 select experiment_slug, experiment_name, experiment_type, experiment_schema, config_data from ab_experiment_v2 where site_id = 2
 and experiment_slug not in 
-('TAS-AI-Instant-Giveaway-QA', 'TAS-Timer-Lander') ;
+('TAS-AI-Instant-Giveaway-QA', 'TAS-Timer-Lander', 'TAS-SS-SW-Single-Select', 'TAS-Dynamic-Flow-WIM-OMG-Sweeps-V2', 'TAS-Instant-Giveaway-5000-3-Step-Reg',
+'') 
+order by experiment_type desc ;
 
--- https://freesamplesprousa.com/?test=1&cid=21eo8&exp=FSPU-Branded-Personalized-Appwall
--- https://theamericansweepstakes.com/?test=1&cid=n9gb2&exp=TAS-Timer-Lander
+-- https://freesamplesprousa.com/?test=1&var=405&cid=21eo8&exp=FSPU-Generic-Pre-filled-Reg
+-- https://theamericansweepstakes.com/?test=1&var=405&cid=n9gb2&exp=TAS-Instant-Giveaway-5000-3-Step-Reg
 
 -- select * from campaign_master where site_id in (1)
 -- and campaign_slug not in ('has03', 'vv3dp') and is_active = 1 ;
